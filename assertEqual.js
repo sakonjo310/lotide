1c02a9e597c836d0629d87c8e990210087adaee8
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-17 22:51:25 +0000

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("❌❌❌ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("✅✅✅ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("light", "light");
+assertEqual("light", "house");
+assertEqual(1, 5);
+assertEqual(1, "1");
