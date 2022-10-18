e6b1ca9f7e115f420d43d33e5dc70bb3f701637c
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-18 23:02:54 +0000

    Add function to check equality

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const checkLength = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    return true;
+  }
+}
+
+const eqArrays = function (arr1, arr2) {
+  if (!checkLength(arr1, arr2)) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
