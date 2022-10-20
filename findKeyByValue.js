cb12e3056b22d07b62dc757eebe3e76ff83e5064
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-20 00:42:59 +0000

    Add function to find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj, val) {
+  let foundKey = undefined;
+  let keys = Object.keys(obj);
+
+  for (let key of keys) {
+    if (obj[key] === val) {
+      foundKey = key;
+    }
+  }
+
+  return foundKey;
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
