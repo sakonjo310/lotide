ddba7495fcf70ea725c8d2ac79ce638e6a4ba894
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-20 23:48:39 +0000

    Create map function

---
+++ map.js
@@ -0,0 +1,40 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const checkLength = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    return true;
+  }
+}
+
+const eqArrays = function (arr1, arr2) {
+  if (!checkLength(arr1, arr2)) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't'])
