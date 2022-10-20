029427db811f91eeb4704ac4a2f7317831ffc453
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-20 02:01:42 +0000

    Fix the object output issue

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -42,9 +42,18 @@ const eqObjects = function (object1, object2) {
 };
 
 const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+
+
+assertObjectsEqual(cd, dc);
+assertObjectsEqual(cd, cd2);
