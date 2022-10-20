372a01d6775774d69c4e9f98335dfd540234e643
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-20 03:46:42 +0000

    Improve function efficiency

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -2,7 +2,7 @@ const checkLength = function (arr1, arr2) {
   if (arr1.length === arr2.length) {
     return true;
   }
-}
+};
 
 const eqArrays = function (arr1, arr2) {
   if (!checkLength(arr1, arr2)) {
@@ -14,15 +14,15 @@ const eqArrays = function (arr1, arr2) {
     }
   }
   return true;
-}
+};
 
 const eqObjects = function (object1, object2) {
+  debugger
   let ob1Keys = Object.keys(object1);
   let ob2Keys = Object.keys(object2);
 
   if (ob1Keys.length !== ob2Keys.length) {
     return false;
-    process.exit();
   }
 
   for (let ob1Key of ob1Keys) {
@@ -31,15 +31,18 @@ const eqObjects = function (object1, object2) {
       if (eqArray) {
         continue;
       }
+      return false;
+
     }
     if (object1[ob1Key] === object2[ob1Key]) {
       continue;
     } else {
       return false;
-      process.exit();
     }
-  } return true;
+  }
+  return true;
 };
+
 
 const assertObjectsEqual = function (actual, expected) {
   const inspect = require('util').inspect;
