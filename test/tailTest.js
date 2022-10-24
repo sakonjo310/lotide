85e87898ea969f71ad5390caee71bf9bf50f57f8
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 20:01:41 +0000

    Implement assert.deepEqual

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,8 +2,12 @@ const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe("#tail", () => {
-  it("returns 2 for [1, 2, 3].length", () => {
-    assert.strictEqual(tail([1, 2, 3]).length, 2);
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns ['light', 'house'] for ['Yoo', 'light', 'house']", () => {
+    assert.deepEqual(tail(['Yoo', 'light', 'house']), ['light', 'house']);
   });
 
 });
