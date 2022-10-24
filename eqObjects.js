8ecb1d43e2d7a36f9eaaa287befab12d210b168b
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 03:35:21 +0000

    Use recursion so it can support sub-objects

--- eqObjects.js
+++ eqObjects.js
@@ -25,34 +25,29 @@ const eqArrays = function (arr1, arr2) {
 }
 
 
-
-
 const eqObjects = function (object1, object2) {
-  debugger
-  let ob1Keys = Object.keys(object1);
-  let ob2Keys = Object.keys(object2);
-
-  if (ob1Keys.length !== ob2Keys.length) {
-    return false;
-  }
-
-  for (let ob1Key of ob1Keys) {
-    if (Array.isArray(object1[ob1Key]) && Array.isArray(object2[ob1Key])) {
-      let eqArray = eqArrays(object1[ob1Key], object2[ob1Key]);
-      if (eqArray) {
+    let ob1Keys = Object.keys(object1);
+    let ob2Keys = Object.keys(object2);
+  
+    if (ob1Keys.length !== ob2Keys.length) {
+      return false;
+    }
+  
+    for (let ob1Key of ob1Keys) {
+      if (Array.isArray(object1[ob1Key]) && Array.isArray(object2[ob1Key])) {
+        return eqArrays(object1[ob1Key], object2[ob1Key]);
+      } else if (typeof(object1[ob1Key]) === 'object' &&  typeof(object2[ob1Key]) === 'object') {
+        return eqObjects(object1[ob1Key], object2[ob1Key]);
+      } else if (object1[ob1Key] === object2[ob1Key]) {
         continue;
+      } else {
+        return false;
       }
-      return false;
+    }
+    return true;
 
-    }
-    if (object1[ob1Key] === object2[ob1Key]) {
-      continue;
-    } else {
-      return false;
-    }
-  }
-  return true;
 };
+
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
@@ -60,3 +55,8 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 
 assertEqual(eqObjects(cd, dc), true)
 assertEqual(eqObjects(cd, cd2), false)
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { b: 2, a: { z: 1 } }), true)
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false)
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)
+
