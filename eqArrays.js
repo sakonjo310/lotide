02556ee21da9377ef2ccb0517b564fcfc4a70297
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 04:51:41 +0000

    Use recursion to support sub-arrays

--- eqArrays.js
+++ eqArrays.js
@@ -13,15 +13,30 @@ const checkLength = function (arr1, arr2) {
 }
 
 const eqArrays = function (arr1, arr2) {
+
+  let result = true;
+
   if (!checkLength(arr1, arr2)) {
     return false;
   }
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      result = eqArrays(arr1[i], arr2[i]);
+    } else if (arr1[i] !== arr2[i]) {
+      result = false;
     }
   }
-  return true;
-}
+
+  return result;
+};
+
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
