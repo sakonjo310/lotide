13e4323d12ba780fff1d0d09c8649b8a5b028066
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 15:47:40 +0000

    Remove test code

--- head.js
+++ head.js
@@ -4,7 +4,3 @@ const head = function(arr) {
   return arr[0];
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
