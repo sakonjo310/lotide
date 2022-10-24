77c531b65940d53821e8d63cf1752b26de059a44
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 23:52:11 +0000

    Create a file to test countOnly function using Mocha and Chai

---
+++ test/countOnlyTest.js
@@ -0,0 +1,29 @@
+const assert = require('chai').assert;
+const countOnly = require('../countOnly');
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+describe("#countOnly", () => {
+
+
+  it("should return { 'Jason': 1, 'Fang': 2 }", () => {
+    assert.deepEqual(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true }), { "Jason": 1, "Fang": 2});
+  });
+
+  it("should return { 'Karl': 1 }", () => {
+    assert.deepEqual(countOnly(firstNames, { "Karl": true, "Paul": true, "Salima": false }), { "Karl" : 1 });
+  });
+
+});
+
