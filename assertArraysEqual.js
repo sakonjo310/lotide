8d9e1b5cd67d370b55c5ad380189873ff027a6ee
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 16:17:21 +0000

    Modularize the functions and remove the test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,4 @@
-const checkLength = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    return true;
-  }
-}
-
-const eqArrays = function (arr1, arr2) {
-  if (!checkLength(arr1, arr2)) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -24,4 +8,6 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
+
+module.exports = assertArraysEqual;
