acf28a7d490cf3c2db749a4dc94d00326f582883
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 20:03:43 +0000

    Implement Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle')
 
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
+describe("#middle", () => {
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+});
+
