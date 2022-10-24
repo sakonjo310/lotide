f67f5e089e924962516d19ffa12ff80df17f3deb
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 15:57:13 +0000

    Separate head test into another file

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,8 @@
 const assertEqual = require('../assertEqual')
 
 
+
+
 //TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
@@ -9,7 +11,4 @@ assertEqual("light", "house");
 assertEqual(1, 5);
 assertEqual(1, "1");
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+
