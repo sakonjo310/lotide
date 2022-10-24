dce7d84b3d50eac2751fea66c9155e37612dd3f2
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 15:56:28 +0000

    Create separate test file

---
+++ test/tailTest.js
@@ -0,0 +1,8 @@
+const tail = require('../tail')
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+const result = tail(words);
+assertEqual(result.length, 2);
