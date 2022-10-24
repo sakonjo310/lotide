9f4070eee996fe69d0c380868524b312dfd09bd9
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 16:08:49 +0000

    Separate test code into a new file

---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays')
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
