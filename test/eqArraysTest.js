7ad7e50ca274ebd8a0ff3a8eb4bf3ccee45bc560
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-24 23:37:02 +0000

    Implement Mocha and Chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,11 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays')
 
+describe("#eqArrays", () => {
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+  it("should return true for ([1, 2, 3], [1, 2, 3])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
+  it("should return true for ([[2, 3], [4]], [[2, 3], [4]])", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
+  it("should return false for ([[2, 3], [4]], [[2, 3], [4, 5]])", () => {
+    assert.strictEqual(eqArrays([[[2, 3], [4]]], [[2, 3], [4, 5]]), false);
+  });
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
+  it("should return false for [[2, 3], [4]], [[2, 3], 4])", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+ 
+});
