5684486dbd2ae4add0a5a068e140f858acb3dead
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-25 01:41:49 +0000

    Modularize the function

--- flatten.js
+++ flatten.js
@@ -1,29 +1,3 @@
-const checkLength = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    return true;
-  }
-}
-
-const eqArrays = function (arr1, arr2) {
-  if (!checkLength(arr1, arr2)) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const flatten = function (arr) {
   let output = [];
   for (let i = 0; i < arr.length; i++) {
@@ -39,4 +13,4 @@ const flatten = function (arr) {
 }
 
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])
+module.exports = flatten;
