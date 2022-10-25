408914470b6753a81597bb58d9efafab5dc5c463
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-25 00:12:14 +0000

    Modularize the function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function (obj, val) {
   let foundKey = undefined;
   let keys = Object.keys(obj);
@@ -19,11 +11,4 @@ const findKeyByValue = function (obj, val) {
   return foundKey;
 }
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
