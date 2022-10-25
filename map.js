8cb9d167c03ffa9cc98554ba2e61eed114ec0e60
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-25 01:58:21 +0000

    Modularize the function

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function (array, callback) {
   const results = [];
   for (let item of array) {
@@ -8,33 +6,6 @@ const map = function (array, callback) {
   return results;
 }
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
 
-const checkLength = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    return true;
-  }
-}
 
-const eqArrays = function (arr1, arr2) {
-  if (!checkLength(arr1, arr2)) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-assertArraysEqual(results1, ['g', 'c', 't', 'm', 't'])
+module.exports = map;
