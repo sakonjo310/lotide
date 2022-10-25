c8af97f5a867197b3fe479cabb4f4886b78c0441
Satoe Sakonjo <sakonjo310@gmail.com>
2022-10-25 01:47:22 +0000

    Modularize the function

--- letterPositions.js
+++ letterPositions.js
@@ -1,29 +1,3 @@
-const checkLength = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    return true;
-  }
-}
-
-const eqArrays = function (arr1, arr2) {
-  if (!checkLength(arr1, arr2)) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const letterPositions = function (sentence) {
   const results = {};
 
@@ -63,6 +37,4 @@ const letterPositions = function (sentence) {
 //   return results;
 // };
 
-
-console.log(letterPositions("lighthouse in the house"))
-assertArraysEqual(letterPositions("hello").e, [1]);
+module.exports = letterPositions;
